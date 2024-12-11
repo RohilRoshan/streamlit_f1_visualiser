0a3493595d96a1ef3956d2f4d08db55da0bc2bbf
Rohil Roshan <rohil.agarwal@lobb.in>
2024-12-11 14:33:17 +0530

    A Base Streamlit F1 Longrun and Telemtry Analyser

---
+++ main_f1.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+import streamlit as st
+
+st.set_page_config(page_title="F1 Dashboard", layout="wide")
+
+st.title("🏎️ Welcome to Rohil's Dashboard")
+st.subheader("Use the sidebar to navigate between pages.")
+st.image("/home/rohil/Documents/personal/Streamlit_app/images/5036625.jpg", caption="Feel the vibes", use_container_width=True)
